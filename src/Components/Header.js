fcc90f0e2af41fc3ffcd284d5c96f3bf930baee9
jaspetham <jaspertham98@gmail.com>
2022-01-11 17:56:35 +0800

    temporary nav

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,18 +2,18 @@ import './Header.css';
 
 const Header = () => {
     return (
-        <header>
+        <header className="flex">
             <div>
                 <img src="../assets/shared/logo.svg" alt="" />
             </div>
-            {/* <nav>
+            <nav>
                 <ul className="primary-navigation underline-indicators flex">
                     <li className="active"><a className="ff-serifs-cond uppercase text-white letter-spacing-2" href="#"><span>00</span>Home</a></li>
                     <li><a className="ff-serifs-cond uppercase text-white letter-spacing-2" href="#"><span>01</span>Destination</a></li>
                     <li><a className="ff-serifs-cond uppercase text-white letter-spacing-2" href="#"><span>02</span>Crew</a></li>
                     <li><a className="ff-serifs-cond uppercase text-white letter-spacing-2" href="#"><span>03</span>Technology</a></li>
                 </ul>
-            </nav> */}
+            </nav>
         </header>
     )
 }
