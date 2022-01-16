ee5f16ea8c152ca2527208186b24b38d40db8902
jaspetham <jaspertham98@gmail.com>
2022-01-16 23:54:51 +0800

    update overall page

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -7,14 +7,14 @@ const Homepage = () => {
         <main id="main" className='homepage'>
             <Header/>
             <div className="grid-container grid-container--home">
-                <div>
+                <div className="home-detail">
                     <h1 className='text-light fs-500 ff-sans-cond uppercase letter-spacing-1'>
                         So, you want to travel to
                         <span className='fs-900 ff-serif text-white d-block'>Space</span>
                     </h1>
                     <p className="text-white">Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience! Explore</p>
                 </div>
-                <div className='align-self-end'>
+                <div className='home-btn-wrap'>
                     <Link to="/destination" className="large-button ff-serif uppercase text-dark bg-white">
                         Explore
                     </Link>
